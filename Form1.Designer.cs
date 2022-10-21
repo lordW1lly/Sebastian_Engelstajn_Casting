f03007885d2d2ccac0ee24eafd6a0aa0efd5b4ed
lordW1lly <sebastianengelstajn@gmail.com>
2022-10-20 22:48:55 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Sebastian_Engelstajn_Casting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttnConvertir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bttnConvertir
+            // 
+            this.bttnConvertir.Location = new System.Drawing.Point(350, 182);
+            this.bttnConvertir.Name = "bttnConvertir";
+            this.bttnConvertir.Size = new System.Drawing.Size(75, 23);
+            this.bttnConvertir.TabIndex = 0;
+            this.bttnConvertir.Text = "Convertir";
+            this.bttnConvertir.UseVisualStyleBackColor = true;
+            this.bttnConvertir.Click += new System.EventHandler(this.bttnConvertir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bttnConvertir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bttnConvertir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sebastian_Engelstajn_Casting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bttnConvertir_Click(object sender, EventArgs e)
+        {
+            int integer = 10;
+            Boolean booleano = true;
+            double gravedad = 9.80;
+            MessageBox.Show(Convert.ToString(integer)); //Conversion de un integer a un string para poder ser visualizado
+            MessageBox.Show(Convert.ToString(booleano));// conversion tmb explicita de boolean --> string
+            MessageBox.Show(Convert.ToString(gravedad));// conversion tmb explicita de double --> string
+        }
+    }
+}
